4b32c5bf819b9996b23ea982ea56aa3f8f041f8f
MikeH <matroxmike@icloud.com>
2017-05-30 15:12:01 +1000

    after some linting

--- e01.hs
+++ e01.hs
@@ -6,8 +6,8 @@ The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 be
 -}
 main = do
   putStrLn " :: Euler problem 01: 3s and 5s ::"
-  putStrLn (" :: Sum of mults of 3&5 < 10: " ++ (show (sum (multiplesOfLessThan [3,5] 10))))
-  putStrLn (" :: Sum of mults of 3&5 < 10: " ++ (show (sum (multiplesOfLessThan [3,5] 1000))))
+  putStrLn (" :: Sum of mults of 3&5 < 10: " ++ show (sum (multiplesOfLessThan [3,5] 10)))
+  putStrLn (" :: Sum of mults of 3&5 < 10: " ++ show (sum (multiplesOfLessThan [3,5] 1000)))
   
   
 
@@ -15,10 +15,10 @@ numbers :: [Integer]
 numbers = numbers' 1
 
 numbers' :: Integer -> [Integer]
-numbers' x = [x]++(numbers' ((fromInteger x)+1))
+numbers' x = x:numbers' (fromInteger x+1)
 
 multiplesOfLessThan :: [Integer] -> Integer -> [Integer]
-multiplesOfLessThan divisors upTo = multiplesOfLessThan' 1 divisors upTo
+multiplesOfLessThan = multiplesOfLessThan' 1 
 
 multiplesOfLessThan' :: Integer -> [Integer] -> Integer -> [Integer]
 multiplesOfLessThan' thisNum divisors upTo
